1dedeca5686231928c04d238ac55ebad390d604a
JustynaBoksz <justynabokszczanin1@gmail.com>
2024-05-05 19:30:41 +0300

    zad3

--- Main.java
+++ Main.java
@@ -7,29 +7,28 @@ public class Main {
     public static void main(String[] args) {
         
         Worker worker1 = new Worker("John", 5500, 1, "2020-01-10", "Software Developer");
-        Worker worker2 = new Worker("Jane", 6000, 2, "2020-02-15", "Senior Developer");
-        Worker worker3 = new Worker("Michael", 5000, 3, "2020-03-20", "Tester");
-        Worker worker4 = new Worker("Eve", 7000, 4, "2020-04-25", "Product Manager");
+        Worker worker2 = new Worker("Jane", 6000, 1, "2020-02-15", "Senior Developer");
+        Worker worker3 = new Worker("Michael", 5000, 2, "2020-03-20", "Tester");
 
         
-        Manager manager = new Manager("George", 10000, 5, "2019-11-01", "Department Head");
+        Manager manager = new Manager("George", 10000, 3, "2019-11-01", "Project Manager");
 
         
         ArrayList<Employee> employees = new ArrayList<>();
         employees.add(worker1);
         employees.add(worker2);
         employees.add(worker3);
-        employees.add(worker4);
         employees.add(manager);
 
         
         for (Employee employee : employees) {
-            employee.work();
-            System.out.println("-" + employee.getName() +
-                               " (ID: " + employee.getId() +
-                               ", Position: " + employee.getPosition() +
-                               ", Hire date: " + employee.getHireDate() +
-                               ", Salary: " + employee.getSalary() + ")");
+            System.out.println(employee.getName() + " has code: " + employee.hashCode());
+        }
+
+        
+        System.out.println("Comparing " + worker2.getName() + " with others:");
+        for (Employee employee : employees) {
+            System.out.println("- " + worker2.getName() + " equals " + employee.getName() + "? " + worker2.equals(employee));
         }
     }
 }
--- company/abstracts/Employee.java
+++ company/abstracts/Employee.java
@@ -35,4 +35,12 @@ public abstract class Employee implements Employable {
         return id;
     }
 
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+
+        Employee employee = (Employee) obj;
+        return id == employee.id;
+    }
 }
